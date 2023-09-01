b74295d510e990c2f699c39c52270edb560453bf
George Guimarães <george.guimaraes@gmail.com>
2023-09-01 13:05:27 -0300

    package is not module. Fixes vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import logseqDevPlugin from "vite-plugin-logseq";
 
 export default defineConfig({
-  plugins: [logseqDevPlugin.default()],
+  plugins: [logseqDevPlugin()],
 });
